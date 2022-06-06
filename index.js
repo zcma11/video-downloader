b84686a3137fc386175771746c9b2bc3ad90a00c
akkraz <919159845@qq.com>
2022-06-07 02:01:17 +0800

    feature: download record,use local file, optimize

--- index.js
+++ index.js
@@ -14,107 +14,166 @@ const CONFIG_DIR = path.join(process.cwd(), 'config')
 const FILE_EXT = '.mp4'
 const FILE_M3U8 = 'media.m3u8'
 const DEFAULT_OUTPUT = './dist'
-const M3U8_URL_REG = /(\/.*?)(?=\"|\n)/g
+const M3U8_URL_REG = /(?<="|\n)([^#\n]+?)(?="|\n)/g
 
-program.option('-c <configFile>').action(async ({ c }) => {
-  let defaultName = 1
-  const config = require(path.join(CONFIG_DIR, c))
-  console.log(config)
-  const {
-    outputDir: _outputDir = DEFAULT_OUTPUT,
-    fileName = defaultName,
-    baseUrl,
-    outputFile
-  } = config
-  const { isExist, existPath, unExistPath } = await checkFile(
-    path.relative(process.cwd(), _outputDir)
-  )
-  console.log(isExist, existPath, unExistPath)
-  if (!isExist) {
-    createDir(unExistPath, existPath)
-  }
+program
+  .option('-c <configFile>')
+  .option('-re, --reload')
+  .action(async ({ c, reload }) => {
+    let defaultName = 1
+    const config = require(path.join(CONFIG_DIR, c))
+    console.log(config)
+    const {
+      outputDir: _outputDir = DEFAULT_OUTPUT,
+      fileName = defaultName,
+      baseUrl,
+      outputFile
+    } = config
+    const outputDir = path.resolve(process.cwd(), _outputDir)
 
-  const { source, fileContent } = await parseSource(config.source)
-  const outputDir = path.resolve(process.cwd(), _outputDir)
-  const fileList = path.resolve(outputDir, FILE_M3U8)
-  console.log(fileContent)
-  await write(fileList, async _write => {
-    let finalContent = ''
-    finalContent = fileContent.replace(M3U8_URL_REG, _ => {
-      return path
-        .resolve(
-          process.cwd(),
-          path.resolve(
-            path.relative(process.cwd(), outputDir),
-            _.split('/').find(s => s.match(fileName))
-          )
-        )
-        .replace(/\\/g, '/') /* for window */
-    })
-    return _write(finalContent)
-  })
+    if (checkFile(outputDir, outputFile).isExist) {
+      console.log(errorColor(`[warn] ${outputFile} is exist`))
+      return
+    }
+    const { isExist, existPath, unExistPath } = checkFile(
+      path.relative(process.cwd(), _outputDir)
+    )
 
-  let count = 0
-  let mission = []
-  for (let src of source) {
-    if (!src.startsWith(http) && config.baseUrl) {
-      let pathName
-      switch (typeof fileName) {
-        case 'number':
-          ++fileName
-          pathName = path.resolve(outputDir, fileName + FILE_EXT)
-          break
-        case 'string':
-          pathName = path.resolve(outputDir, fileName)
-          break
-        case 'object':
-          if (isRegExp(fileName)) {
-            pathName = path.resolve(
-              outputDir,
-              src.split('/').find(s => s.match(fileName))
+    if (!isExist) {
+      createDir(unExistPath, existPath)
+    }
+
+    const { source, fileContent } = await parseSource(config.source)
+    if (!source.length || !fileContent) return
+
+    const fileList = path.resolve(outputDir, FILE_M3U8)
+
+    await write(fileList, async _write => {
+      let finalContent = ''
+      finalContent = fileContent.replace(M3U8_URL_REG, _ => {
+        return path
+          .resolve(
+            process.cwd(),
+            path.resolve(
+              path.relative(process.cwd(), outputDir),
+              _.split('/').find(s => s.match(fileName))
             )
-          }
-          break
-        default:
-          throw new Error('fileName must be a string but got ', fileName)
-      }
+          )
+          .replace(/\\/g, '/') /* for window */
+      })
+      return _write(finalContent.trim())
+    })
 
-      const task = () =>
-        write(pathName, _write => {
-          return download(baseUrl + src, () => _write)
-        }).finally(() => {
+    let count = 0
+    const fufillFile = path.resolve(outputDir, 'fulfill.txt')
+    const failureFile = path.resolve(outputDir, 'failure.txt')
+    const mission = []
+    const fulfill =
+      !reload && checkFile(fufillFile).isExist
+        ? [...new Set(fs.readFileSync(fufillFile, 'utf8').split('\n'))]
+        : []
+    const failure = []
+
+    for (let src of source) {
+      if (fulfill.includes(src)) {
+        continue
+      }
+      if (baseUrl) {
+        // download and merge online file
+        let pathName
+        switch (typeof fileName) {
+          case 'number':
+            ++fileName
+            pathName = path.resolve(outputDir, fileName + FILE_EXT)
+            break
+          case 'string':
+            pathName = path.resolve(outputDir, fileName)
+            break
+          case 'object':
+            if (isRegExp(fileName)) {
+              pathName = path.resolve(
+                outputDir,
+                src.split('/').find(s => s.match(fileName))
+              )
+            }
+            break
+          default:
+            throw new Error('fileName must be a string but got ', fileName)
+        }
+
+        const url = baseUrl + src
+        const task = async () => {
+          const res = await write(pathName, async _write => {
+            return download(url, () => _write)
+              .then(_ => {
+                fulfill.push(url)
+                return _
+              })
+              .catch(_ => {
+                failure.push(url)
+                return _
+              })
+          })
+
           console.log(
             `${pathName} is over. schedule: ${++count}/${source.length}`
           )
-        })
 
-      mission.push(task)
-    }
-  }
-
-  const thead = useThead(mission, 5)
-
-  await thead.work()
-
-  console.log(`try to convert ${FILE_M3U8} to ${outputFile}`)
-
-  await new Promise(resolve => {
-    childProcess
-      .exec(
-        `cd ${outputDir} && ffmpeg -i ${FILE_M3U8} -c copy ${outputFile}`,
-        (err, stdout) => {
-          if (err) {
-            console.log(err + '\nfail to convert m3u8')
-          } else {
-            console.log(stdout)
-          }
+          return res
         }
-      )
-      .on('close', resolve)
-  })
 
-  console.log('\n**** finish ****')
-})
+        mission.push(task)
+      } else {
+        // only merge local file
+        fulfill.push(src)
+      }
+    }
+
+    const thead = useThead(mission, 5)
+
+    await thead.work()
+
+    const logFile = (file, content) => {
+      write(file, async _write => {
+        _write(content.join('\n'))
+      })
+    }
+
+    if (fulfill.length) {
+      logFile(fufillFile, fulfill)
+    }
+
+    if (failure.length) {
+      logFile(failureFile, failure)
+    }
+
+    if (failure.length || !fulfill.length) {
+      process.exit(0)
+    }
+    console.log(`\ntry to convert ${FILE_M3U8} to ${outputFile}`)
+
+    await new Promise(resolve => {
+      childProcess
+        .exec(
+          `cd ${outputDir} && ffmpeg -i ${FILE_M3U8} -c copy ${outputFile}`,
+          (err, stdout) => {
+            if (err) {
+              console.log(err + '\nfail to convert m3u8')
+            } else {
+              console.log(stdout)
+              console.log('creating...')
+            }
+          }
+        )
+        .on('close', () => {
+          setTimeout(() => {
+            resolve()
+          }, 1000)
+        })
+    })
+
+    console.log('\n**** finish ****')
+  })
 
 program.configureOutput({
   outputError: (str, write) => write(errorColor(str))
@@ -135,7 +194,7 @@ function parseSource(source) {
         source = res.result
       } else if (isString(source)) {
         const file = path.resolve(process.cwd(), source)
-        const { isExist } = await checkFile(file)
+        const { isExist } = checkFile(file)
         if (!isExist) {
           return
         }
@@ -143,6 +202,7 @@ function parseSource(source) {
         source = fs.readFileSync(file, 'utf8')
       }
 
+      console.log(source.match(M3U8_URL_REG))
       const urlList = source.match(M3U8_URL_REG) || []
       resolve({ source: urlList, fileContent: source })
     } catch (error) {
@@ -153,23 +213,28 @@ function parseSource(source) {
 }
 
 function download(url, action) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, _reject) => {
     let request
     switch (url.match(/https?/)[0]) {
       case 'http':
-        request = http.get(url)
+        request = http.get(url, { timeout: 10000 })
       case 'https':
-        request = https.get(url)
+        request = https.get(url, { timeout: 10000 })
     }
 
-    request.on('error', err => {
-      reject(err)
-    })
+    const reject = _ => {
+      _reject(_)
+    }
 
-    request.on('response', response => {
-      let res = {}
-      response.on('data', action(res))
-      response.on('end', () => resolve(res))
-    })
+    request
+      .on('error', reject)
+      .on('timeout', reject)
+      .on('response', response => {
+        let res = {}
+        response
+          .on('data', action(res))
+          .on('end', () => resolve(res))
+          .on('error', reject)
+      })
   })
 }

fc0f833c8b735ded58051184c13f304a7b9241bf
akkraz <919159845@qq.com>
2022-06-06 18:29:13 +0800

    feture add ffmpeg

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
 #!/usr/bin/env node
-// import { createWriteStream, readFile } from 'fs'
-// import { request } from 'http'
-// import { join } from 'path'
-
-// const listName = ''
-// const fileName = ''
-// const outputDir = ''
-// const baseUrl = 'https://dl193248.twitcasting.tv'
-
-// readFile(Path.join(process.cwd, listName))
-
-// const download = (fileName, output, urls) => {}
-const fs = require('fs')
 const path = require('path')
+const fs = require('fs')
 const http = require('http')
 const https = require('https')
-const { errorColor, isRegExp } = require('./lib/utils')
-const { checkFile, createDir } = require('./lib/fs')
+const childProcess = require('child_process')
+const { errorColor, isRegExp, isString } = require('./lib/utils')
+const { checkFile, createDir, write } = require('./lib/fs')
 const useThead = require('./lib/useThead')
 const { Command } = require('commander')
 const program = new Command()
 
-const configDir = path.join(process.cwd(), 'config')
-const fileExt = '.mp4'
+const CONFIG_DIR = path.join(process.cwd(), 'config')
+const FILE_EXT = '.mp4'
+const FILE_M3U8 = 'media.m3u8'
+const DEFAULT_OUTPUT = './dist'
+const M3U8_URL_REG = /(\/.*?)(?=\"|\n)/g
 
-// program
-//   .command('download <url>')
-//   .description('请输入 URL')
-//   .action(url => {
-//     console.log(url)
-//     getConfig(url)
-//   })
-// .error('Please fill in the URL')
 program.option('-c <configFile>').action(async ({ c }) => {
   let defaultName = 1
-  const config = require(path.join(configDir, c))
+  const config = require(path.join(CONFIG_DIR, c))
   console.log(config)
-  let { outputDir = './dist', fileName = defaultName, baseUrl } = config
+  const {
+    outputDir: _outputDir = DEFAULT_OUTPUT,
+    fileName = defaultName,
+    baseUrl,
+    outputFile
+  } = config
   const { isExist, existPath, unExistPath } = await checkFile(
-    config.outputDir
-    // config.fileName || 'media.21.mp4'
+    path.relative(process.cwd(), _outputDir)
   )
   console.log(isExist, existPath, unExistPath)
   if (!isExist) {
     createDir(unExistPath, existPath)
   }
 
-  const source = await parseSource(config.source)
-  console.log(source)
+  const { source, fileContent } = await parseSource(config.source)
+  const outputDir = path.resolve(process.cwd(), _outputDir)
+  const fileList = path.resolve(outputDir, FILE_M3U8)
+  console.log(fileContent)
+  await write(fileList, async _write => {
+    let finalContent = ''
+    finalContent = fileContent.replace(M3U8_URL_REG, _ => {
+      return path
+        .resolve(
+          process.cwd(),
+          path.resolve(
+            path.relative(process.cwd(), outputDir),
+            _.split('/').find(s => s.match(fileName))
+          )
+        )
+        .replace(/\\/g, '/') /* for window */
+    })
+    return _write(finalContent)
+  })
+
   let count = 0
   let mission = []
   for (let src of source) {
@@ -56,17 +62,16 @@ program.option('-c <configFile>').action(async ({ c }) => {
       switch (typeof fileName) {
         case 'number':
           ++fileName
-          pathName = path.resolve(process.cwd(), outputDir, fileName + fileExt)
+          pathName = path.resolve(outputDir, fileName + FILE_EXT)
           break
         case 'string':
-          pathName = path.resolve(process.cwd(), outputDir, fileName)
+          pathName = path.resolve(outputDir, fileName)
           break
         case 'object':
           if (isRegExp(fileName)) {
             pathName = path.resolve(
-              process.cwd(),
               outputDir,
-              src.split('/').find(s => fileName.exec(s))
+              src.split('/').find(s => s.match(fileName))
             )
           }
           break
@@ -74,27 +79,16 @@ program.option('-c <configFile>').action(async ({ c }) => {
           throw new Error('fileName must be a string but got ', fileName)
       }
 
-      const p = () => {
-        const writeStream = fs.createWriteStream(pathName)
-        writeStream.on('error', e => {
-          writeStream.end()
-        })
-
-        return download(baseUrl + src, () => data => {
-          writeStream.write(data)
-        })
-          .then(
-            () => writeStream.end(),
-            () => writeStream.end()
+      const task = () =>
+        write(pathName, _write => {
+          return download(baseUrl + src, () => _write)
+        }).finally(() => {
+          console.log(
+            `${pathName} is over. schedule: ${++count}/${source.length}`
           )
-          .finally(() => {
-            console.log(
-              `${pathName} is over. schedule: ${++count}/${source.length}`
-            )
-          })
-      }
+        })
 
-      mission.push(p)
+      mission.push(task)
     }
   }
 
@@ -102,7 +96,24 @@ program.option('-c <configFile>').action(async ({ c }) => {
 
   await thead.work()
 
-  console.log('finish')
+  console.log(`try to convert ${FILE_M3U8} to ${outputFile}`)
+
+  await new Promise(resolve => {
+    childProcess
+      .exec(
+        `cd ${outputDir} && ffmpeg -i ${FILE_M3U8} -c copy ${outputFile}`,
+        (err, stdout) => {
+          if (err) {
+            console.log(err + '\nfail to convert m3u8')
+          } else {
+            console.log(stdout)
+          }
+        }
+      )
+      .on('close', resolve)
+  })
+
+  console.log('\n**** finish ****')
 })
 
 program.configureOutput({
@@ -110,55 +121,38 @@ program.configureOutput({
 })
 program.parse(process.argv)
 
-// const { program } = require('commander')
-// download()
-
-// function getConfig(fileName) {
-//   return new Promise((resolve, reject) => {
-//     if (fileName.startsWith('http')) {
-//       return download(fileName)
-//     }
-//     fs.readFile(`./config/${fileName}`, 'utf8', (err, data) => {
-//       console.log(data)
-//       resolve(JSON.parse(data))
-//     })
-//   })
-// }
-
 function parseSource(source) {
   return new Promise(async resolve => {
-    if (source.startsWith('http')) {
-      try {
+    try {
+      if (source.startsWith('http')) {
         const res = await download(source, res => {
           res.result = ''
           return data => {
             res.result += data.toString()
           }
         })
-        source = res.result.match(/(\/.*?)(?=\"|\n)/g)
-        // resolve(res)
-        console.log(source)
-        if (!source) {
-          resolve([])
+
+        source = res.result
+      } else if (isString(source)) {
+        const file = path.resolve(process.cwd(), source)
+        const { isExist } = await checkFile(file)
+        if (!isExist) {
+          return
         }
-        resolve(source)
-      } catch (error) {
-        console.log(error)
-        resolve([])
+
+        source = fs.readFileSync(file, 'utf8')
       }
+
+      const urlList = source.match(M3U8_URL_REG) || []
+      resolve({ source: urlList, fileContent: source })
+    } catch (error) {
+      console.log(error)
+      resolve({ source: [], fileContent: '' })
     }
-    // fs.readFile('./media.m3u8', 'utf8', (err, data) => {
-    //   if (err) {
-    //     reject(err)
-    //   }
-    //   console.log(typeof data)
-    //   const r =
-    //   resolve(r)
-    // })
   })
 }
 
-function download(url, cb) {
+function download(url, action) {
   return new Promise((resolve, reject) => {
     let request
     switch (url.match(/https?/)[0]) {
@@ -174,7 +168,7 @@ function download(url, cb) {
 
     request.on('response', response => {
       let res = {}
-      response.on('data', cb(res))
+      response.on('data', action(res))
       response.on('end', () => resolve(res))
     })
   })

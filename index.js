12a00953932b45adf1f1da8820a8742756a01bf5
akkraz <919159845@qq.com>
2022-06-17 18:46:26 +0800

    feature: suport config[]

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const fs = require('fs')
 const http = require('http')
 const https = require('https')
 const childProcess = require('child_process')
-const { errorColor, isRegExp, isString, formatM3U8Url } = require('./lib/utils')
+const {
+  errorColor,
+  isRegExp,
+  isString,
+  formatM3U8Url,
+  isArray
+} = require('./lib/utils')
 const { checkFile, createDir, write } = require('./lib/fs')
 const useThead = require('./lib/useThead')
 const { Command } = require('commander')
@@ -23,192 +29,213 @@ program
   .option('-c <configFile>')
   .option('-re, --reload')
   .action(async ({ c, reload }) => {
-    let defaultName = 1
-    const config = require(path.join(CONFIG_DIR, c))
+    let config = require(path.join(CONFIG_DIR, c))
     console.log(config)
-    const {
-      outputDir: _outputDir = DEFAULT_OUTPUT,
-      fileName = defaultName,
-      baseUrl,
-      outputFile
-    } = config
-    const outputDir = path.resolve(process.cwd(), _outputDir)
 
-    if (checkFile(outputDir, outputFile).isExist) {
-      console.log(errorColor(`[warn] ${outputFile} is exist`))
-      return
-    }
-    const { isExist, existPath, unExistPath } = checkFile(
-      path.relative(process.cwd(), _outputDir)
-    )
-
-    if (!isExist) {
-      createDir(unExistPath, existPath)
+    if (!isArray(config)) {
+      config = [config]
     }
 
-    const originSource = await parseSource(config.source)
-    let source = originSource.source
-    let fileContent = originSource.fileContent
-
-    if (!source.length || !fileContent) {
-      console.log(errorColor('[error] can not find source'))
-      return
-    }
-
-    // handle .key v1.1
-    if (hasKey(fileContent)) {
+    for (let i = 0; i < config.length; i++) {
       try {
-        const afterProcessing = await parseKey(source, fileContent, outputDir)
-        source = afterProcessing.source
-        fileContent = afterProcessing.fileContent
+        const conf = config[i]
+        console.log(`[vd] handle ${conf.outputFile}`)
+        await main({ c, reload }, conf)
+        console.log(
+          `[vd] finish ${conf.outputFile} (${i + 1}/${config.length})`
+        )
       } catch (error) {
         console.log(
-          errorColor('[error] fail to create .key\n  ') + error + '\n'
-        )
-      }
-    }
-
-    const fileList = path.resolve(outputDir, FILE_M3U8)
-
-    try {
-      console.log('save .m3u8\n')
-      await write(fileList, async _write => {
-        let finalContent = ''
-        finalContent = fileContent.replace(M3U8_URL_REG, _ => {
-          const srcLine = _.split('/').find(s => s.match(fileName))
-          if (!srcLine) {
-            return _
-          }
-
-          return formatM3U8Url(
-            path.resolve(
-              process.cwd(),
-              path.resolve(path.relative(process.cwd(), outputDir), srcLine)
-            )
+          errorColor(
+            `[error] Encountered an unknown error. ${conf.outputFile} config[${i}]\n${error}`
           )
-        })
-
-        return _write(finalContent.trim())
-      })
-    } catch (error) {
-      console.log(errorColor('[warn] fail to save .m3u8\n  ' + error))
-    }
-
-    let count = 0
-    const fufillFile = path.resolve(outputDir, 'fulfill.txt')
-    const failureFile = path.resolve(outputDir, 'failure.txt')
-    const mission = []
-    const fulfill =
-      !reload && checkFile(fufillFile).isExist
-        ? [...new Set(fs.readFileSync(fufillFile, 'utf8').split('\n'))]
-        : []
-    const failure = []
-    for (let _src of source) {
-      const src = baseUrl ? baseUrl + _src : _src
-
-      if (fulfill.includes(src)) {
-        console.log(
-          `${src} is downloaded. schedule: ${++count}/${source.length}`
         )
-        continue
-      }
-
-      if (baseUrl) {
-        // download and merge online file
-        let pathName
-        switch (typeof fileName) {
-          case 'number':
-            ++fileName
-            pathName = path.resolve(outputDir, fileName + FILE_EXT)
-            break
-          case 'string':
-            pathName = path.resolve(outputDir, fileName)
-            break
-          case 'object':
-            if (isRegExp(fileName)) {
-              pathName = path.resolve(
-                outputDir,
-                src.split('/').find(s => s.match(fileName))
-              )
-            }
-            break
-          default:
-            throw new Error('fileName must be a string but got ', fileName)
-        }
-
-        const url = src
-        const task = async () => {
-          const res = await write(pathName, async _write => {
-            return download(url, () => _write)
-              .then(_ => {
-                fulfill.push(url)
-                return _
-              })
-              .catch(_ => {
-                failure.push(url)
-                return _
-              })
-          })
-
-          console.log(
-            `${pathName} is over. schedule: ${++count}/${source.length}`
-          )
-
-          return res
-        }
-
-        mission.push(task)
-      } else {
-        // only merge local file
-        fulfill.push(src)
       }
     }
-
-    const thead = useThead(mission, 5)
-
-    await thead.work()
-
-    const logFile = (file, content) => {
-      write(file, async _write => {
-        _write(content.join('\n'))
-      })
-    }
-
-    if (fulfill.length) {
-      logFile(fufillFile, fulfill)
-    }
-
-    if (failure.length) {
-      logFile(failureFile, failure)
-    }
-
-    if (failure.length || !fulfill.length) {
-      process.exit(0)
-    }
-    console.log(`\ntry to convert ${FILE_M3U8} to ${outputFile}`)
-
-    await new Promise(resolve => {
-      childProcess
-        .exec(
-          `cd ${outputDir} && ffmpeg -allowed_extensions ALL -i ${FILE_M3U8} -c copy ${outputFile}`,
-          (err, stdout) => {
-            if (err) {
-              console.log(errorColor(err + '\nfail to convert m3u8\n'))
-            } else {
-              console.log(stdout)
-              console.log('creating...')
-            }
-          }
-        )
-        .on('close', () => {
-          setTimeout(() => {
-            resolve()
-          }, 1000)
-        })
-    })
-
-    console.log('\n**** finish ****')
   })
+
+async function main({ reload }, config) {
+  let defaultName = 1
+  const {
+    outputDir: _outputDir = DEFAULT_OUTPUT,
+    fileName = defaultName,
+    baseUrl,
+    outputFile
+  } = config
+  const outputDir = path.resolve(process.cwd(), _outputDir)
+
+  if (checkFile(outputDir, outputFile).isExist) {
+    console.log(errorColor(`[warn] ${outputFile} is exist`))
+    return
+  }
+  const { isExist, existPath, unExistPath } = checkFile(
+    path.relative(process.cwd(), _outputDir)
+  )
+
+  if (!isExist) {
+    createDir(unExistPath, existPath)
+  }
+
+  const originSource = await parseSource(config.source)
+  let source = originSource.source
+  let fileContent = originSource.fileContent
+
+  if (!source.length || !fileContent) {
+    console.log(errorColor('[error] can not find source'))
+    return
+  }
+
+  // handle .key v1.1
+  const IS_ENCRYPTION = hasKey(fileContent)
+  if (IS_ENCRYPTION) {
+    try {
+      const afterProcessing = await parseKey(source, fileContent, outputDir)
+      source = afterProcessing.source
+      fileContent = afterProcessing.fileContent
+    } catch (error) {
+      console.log(errorColor('[error] fail to create .key\n  ') + error + '\n')
+    }
+  }
+
+  const fileList = path.resolve(outputDir, FILE_M3U8)
+
+  try {
+    console.log('save .m3u8\n')
+    await write(fileList, async _write => {
+      let finalContent = ''
+      finalContent = fileContent.replace(M3U8_URL_REG, _ => {
+        const srcLine = _.split('/').find(s => s.match(fileName))
+        if (!srcLine) {
+          return _
+        }
+
+        return formatM3U8Url(
+          path.resolve(
+            process.cwd(),
+            path.resolve(path.relative(process.cwd(), outputDir), srcLine)
+          )
+        )
+      })
+
+      return _write(finalContent.trim())
+    })
+  } catch (error) {
+    console.log(errorColor('[warn] fail to save .m3u8\n  ' + error))
+  }
+
+  let count = 0
+  const fufillFile = path.resolve(outputDir, 'fulfill.txt')
+  const failureFile = path.resolve(outputDir, 'failure.txt')
+  const mission = []
+  const fulfill =
+    !reload && !IS_ENCRYPTION && checkFile(fufillFile).isExist
+      ? [...new Set(fs.readFileSync(fufillFile, 'utf8').split('\n'))]
+      : []
+  const failure = []
+  for (let _src of source) {
+    const src = baseUrl ? baseUrl + _src : _src
+
+    if (fulfill.includes(src)) {
+      console.log(`${src} is downloaded. schedule: ${++count}/${source.length}`)
+      continue
+    }
+
+    if (baseUrl) {
+      // download and merge online file
+      let pathName
+      switch (typeof fileName) {
+        case 'number':
+          ++fileName
+          pathName = path.resolve(outputDir, fileName + FILE_EXT)
+          break
+        case 'string':
+          pathName = path.resolve(outputDir, fileName)
+          break
+        case 'object':
+          if (isRegExp(fileName)) {
+            pathName = path.resolve(
+              outputDir,
+              src.split('/').find(s => s.match(fileName))
+            )
+          }
+          break
+        default:
+          throw new Error('fileName must be a string but got ', fileName)
+      }
+
+      const url = src
+      const task = async () => {
+        const res = await write(pathName, async _write => {
+          return download(url, () => _write)
+            .then(_ => {
+              fulfill.push(url)
+              return _
+            })
+            .catch(_ => {
+              failure.push(url)
+              return _
+            })
+        })
+
+        console.log(
+          `${pathName} is over. schedule: ${++count}/${source.length}`
+        )
+
+        return res
+      }
+
+      mission.push(task)
+    } else {
+      // only merge local file
+      fulfill.push(src)
+    }
+  }
+
+  const thead = useThead(mission, 5)
+
+  await thead.work()
+
+  const logFile = (file, content) => {
+    write(file, async _write => {
+      _write(content.join('\n'))
+    })
+  }
+
+  if (fulfill.length) {
+    logFile(fufillFile, fulfill)
+  }
+
+  if (failure.length) {
+    logFile(failureFile, failure)
+  }
+
+  if (failure.length || !fulfill.length) {
+    process.exit(0)
+  }
+  console.log(`\ntry to convert ${FILE_M3U8} to ${outputFile}`)
+
+  await new Promise(resolve => {
+    childProcess
+      .exec(
+        `cd ${outputDir} && ffmpeg -allowed_extensions ALL -i ${FILE_M3U8} -c copy ${outputFile}`,
+        (err, stdout) => {
+          if (err) {
+            console.log(errorColor(err + '\nfail to convert m3u8\n'))
+          } else {
+            console.log(stdout)
+            console.log('creating...')
+          }
+        }
+      )
+      .on('close', () => {
+        setTimeout(() => {
+          resolve()
+        }, 1000)
+      })
+  })
+
+  console.log('\n**** finish ****')
+}
 
 program.configureOutput({
   outputError: (str, write) => write(errorColor(str))
